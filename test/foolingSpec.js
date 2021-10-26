b53636768670e054f9508191c75be625f074b8bb
Tom Baxter <thomas99baxter@gmail.com>
2021-10-26 11:30:45 +0100

    getMenu

--- test/foolingSpec.js
+++ test/foolingSpec.js
@@ -1,10 +1,16 @@
 const {expect} = require('chai')
-const Test = require('../lib/fooling')
+const Takeaway = require('../lib/Takeaway')
 
 describe('Test', () => {
-  it('it should give the name back', () => {
-    const myTest = new Test("thomas")
-
-    expect(myTest.getName()).to.eq("thomas")
-  });
+  it("", () => {
+    testTakeaway = new Takeaway();
+    expect(testTakeaway.getMenu()).to.be.an.instanceof(Object)
+    expect(testTakeaway.getMenu()).to.deep.eq(
+      {
+        pizzas: { pepperoni: 8, margherita: 6, beef: 7.5 },
+        wraps: { chicken: 8, lamb: 3, shish: 5 },
+        sides: { salad: 2.2, chips: 2, sauce: 0.8 }
+      }
+    )
+  })
 })
